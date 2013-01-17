7a694601302a2640b0ff68387a1976b4db8bab9c
Greg Boone <boone.greg@gmail.com>
2013-01-17 11:12:46 -0500

    bugfix
    fixed a bug wherein all inputs would yield and error.

--- 0.9.0/beer-ajax.php
+++ 0.9.0/beer-ajax.php
@@ -1,9 +1,14 @@
 <?php //if (isset($_POST[submit]){unset($_POST[submit]);}
 
 if (isset($_POST)) {
-	$abv = $_POST[abvc]/100;
-	$alcoz = $abv*$_POST[size];
-	$beernum = $alcoz / $_POST[cost];
+	$abvc = strip_tags(intval($_POST[abvc]));
+	$size = strip_tags(intval($_POST[size]));
+	$cost = strip_tags(intval($_POST[cost]));
+	$beer = $_POST[name];
+	$abv = $abvc/100;
+	$alcoz = $abv*$size;
+	$beernum = $alcoz/$cost;
+	
 } ?>
   <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
@@ -19,7 +24,8 @@ if (isset($_POST)) {
 		
 	print_r("<p>" . $beer . " at $" . $cost . " has a beer value of " . round(100*$beernum,2) . "</p>");
 		
-	}?>
+	}
+	?>
 		
 	<div class="alert alert-info"><a href="about-beer.php#this-number">What does this number mean?</a></div>
   </div>
